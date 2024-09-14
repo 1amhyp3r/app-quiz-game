f3dc047434de773d3b7e7d764a42d60a1415e659
farid <fajaffarov@gmail.com>
2024-09-14 12:38:43 +0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Quiz Game Project
+This project is a terminal-based quiz game.
---
+++ quiz-game.py
@@ -0,0 +1,20 @@
+from questions import questions
+
+
+def run_quiz(questions):
+    score = 0
+    for question in questions:
+        print(question["prompt"])
+        for option in question["options"]:
+            print(option)
+        answer = input("Enter your answer (A, B, C, or D): ").upper()
+        if answer == question["answer"]:
+            print("Correct, hooray!!\n")
+            score += 1
+        else:
+            print(f"Wrong! The correct answer was {question["answer"]}\n")
+
+    print(f"You got {score} out of {len(questions)} questions correct.")
+
+
+run_quiz(questions)
